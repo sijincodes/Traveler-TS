5e42fa2e179680a9d5620c46b081797ac6caea0a
sijincodes <sijinmathews03@gmail.com>
2023-01-13 10:26:09 +0100

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 
-import logo from "../images/logo.png"
+import logo from "../images/logo.png";
+import menu from "../images/menu.svg";
+import PopupMenu from "./PopupMenu";
 
-function Navbar({navlinks}) {
+function Navbar({ navlinks }) {
+  const [popupState, setPopupState] = useState(false)
+  const onTriggerPopup = () =>{
+    setPopupState(!popupState)
+  }
   return (
-    <header className="flex items-center justify-center w-auto h-auto absolute top-0 left-0 right-0">
-      <nav className="flex items-center justify-between traveler-container">
-        <NavLink to={`/`} className="flex items-center">
-          <img src={logo} alt="logo" className="w-22 h-9 object-fill" />
-        </NavLink>
-        <ul className="flex items-center lg:hidden gap-7">
-         {navlinks?.map((elm,index)=> <li key={index}><NavLink className="text-lg text-slate-900"to={`#`}>{elm.link}</NavLink></li>) }
-        </ul>
-        <ul className="flex items-center lg:hidden">
-          <li><button className="button-green px-7 text-base"type="button">Join Us</button></li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className="flex items-center justify-center w-auto h-auto absolute top-7 left-0 right-0">
+        <nav className="flex items-center justify-between traveler-container">
+          <NavLink to={`/`} className="flex items-center">
+            <img src={logo} alt="logo" className="w-22 h-9 object-fill" />
+          </NavLink>
+          <ul className="flex items-center lg:hidden gap-7">
+            {navlinks?.map((elm, index) => (
+              <li key={index}>
+                <NavLink className="text-lg text-slate-900" to={`#`}>
+                  {elm.link}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+          <ul className="flex items-center lg:hidden">
+            <li>
+              <button className="button-green px-7 text-base" type="button">
+                Join Us
+              </button>
+            </li>
+          </ul>
+          <ul className="hidden lg:flex items-center">
+            <li>
+              <button
+                type="button"
+                className="flex items-center justify-center transition-all duration-200 active:scale-90 cursor-pointer"
+                 onClick={onTriggerPopup}
+              >
+                <img
+                  className="object-cover shadown-sm filter"
+                  src={menu}
+                  alt="menu-icon"
+                ></img>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <PopupMenu navlinks={navlinks} popupState={popupState} />
+    </>
   );
 }
 
---
+++ src/components/PopupMenu.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { NavLink } from "react-router-dom";
+
+function PopupMenu({navlinks, popupState}) {
+  return (
+    <>
+        <nav className={`fixed top-14 right-14 bg-white bg-opacity-70 opacity-100
+         z-50 backdrop-blur-md backdrop-filter rounded-lg h-auto w-44 py-5 px-6 hidden lg:flex items-center justify-center transition-transform duration-300 ;
+         ${popupState ? 'lg:showpopup':'lg:noshowpopup'}`}>
+            <ul className='flex items-start flex-col gap-3'>
+                {navlinks?.map((elm,index) => (
+                    <li key={index}><NavLink className="text-lg text-slate-900" to={`#`}>
+                {elm.link}
+              </NavLink>
+              </li>
+                ))}
+                <li>
+              <button className="button-light sm:w-auto shadow-slate-300 rounded-lg" type="button">
+                Join Us
+              </button>
+            </li>
+            </ul>
+        </nav>
+    </>
+  )
+}
+
+export default PopupMenu
